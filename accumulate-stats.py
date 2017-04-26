91fbf0686dd63ab89408e3719c47de0fda007777
Kendall Weihe <kendallweihe@gmail.com>
2017-04-26 12:24:03 -0400

    added csv

--- accumulate-stats.py
+++ accumulate-stats.py
@@ -2,37 +2,43 @@ import numpy as np
 import pdb
 import os
 
-directories = os.listdir("./seasons/")
+directories = os.listdir("/home/kendall/Development/nba-basketball-db/advanced-seasons/")
 
 for d in directories:
-    files = os.listdir("./seasons/"+d)
-    for f in files:
-        data = np.genfromtxt("./seasons/"+d+"/"+f, delimiter=",")
+    if d != "2012":
+        files = os.listdir("/home/kendall/Development/nba-basketball-db/advanced-seasons/"+d)
+        for f in files:
+            data = np.genfromtxt("/home/kendall/Development/nba-basketball-db/advanced-seasons/"+d+"/"+f, delimiter=",")
 
-        if data.shape[0] == 47:
-            continue
+            if data.shape[0] == 47:
+                continue
 
-        # if d == "2017" and f == "Gonzaga.csv":
-        #     pdb.set_trace()
-        #     print
+            # if d == "2017" and f == "Golden-State.csv":
+            #     pdb.set_trace()
+            #     print
 
-        data = data[::-1,:]
+            try:
+                data = data[::-1,:]
+            except:
+                pdb.set_trace()
+                print
 
-        accumulated_data = []
-        for row in range(1,data.shape[0]+1):
-            # accumulated_data.append(np.mean(data[0:row,:], axis=0))
-            row_data = []
-            for column in range(data.shape[1]):
-                if column != 0 and column != 46 and column != 43:
-                    row_data.append(np.mean(data[0:row,column]))
-                else:
-                    row_data.append(data[row-1,column])
-            accumulated_data.append(row_data)
+            accumulated_data = []
+            for row in range(1,data.shape[0]+1):
+                # accumulated_data.append(np.mean(data[0:row,:], axis=0))
+                row_data = []
+                for column in range(data.shape[1]):
+                    # if column != 0 and column != 46 and column != 43:
+                    if column != 0 and column != 29 and column != 32:
+                        row_data.append(np.mean(data[0:row,column]))
+                    else:
+                        row_data.append(data[row-1,column])
+                accumulated_data.append(row_data)
 
-        accumulated_data = np.array(accumulated_data)[::-1,:]
+            accumulated_data = np.array(accumulated_data)[::-1,:]
 
-        try:
-            os.mkdir("./accumulated/"+d)
-        except:
-            pass
-        np.savetxt("./accumulated/"+d+"/"+f, accumulated_data, delimiter=",")
+            try:
+                os.mkdir("/home/kendall/Development/nba-basketball-db/advanced-accumulated/"+d)
+            except:
+                pass
+            np.savetxt("/home/kendall/Development/nba-basketball-db/advanced-accumulated/"+d+"/"+f, accumulated_data, delimiter=",")
